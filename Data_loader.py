d7d4a1dd88b0765b8863f2a8f6e9339efd14843a
Karl-Liu-ch <liu_ht2010@qq.com>
2022-11-28 22:15:43 +0100

    dataloader

--- Data_loader.py
+++ Data_loader.py
@@ -8,21 +8,21 @@ from torchvision.datasets import CIFAR10, MNIST, FashionMNIST
 import torchvision.transforms as transforms
 
 # Define the train and test sets
-dataset_cifar = CIFAR10(root="../Dataset/", download=True,
+dataset_cifar = CIFAR10(root="Dataset/", download=True,
                            transform=transforms.Compose([
                                transforms.Resize(32),
                                transforms.CenterCrop(32),
                                transforms.ToTensor(),
                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                            ]))
-dataset_mnist = MNIST(root="../Dataset/", download=True,
+dataset_mnist = MNIST(root="Dataset/", download=True,
                            transform=transforms.Compose([
                                transforms.Resize(28),
                                transforms.CenterCrop(28),
                                transforms.ToTensor(),
                                transforms.Normalize((0.5,), (0.5,)),
                            ]))
-dataset_fashionmnist = FashionMNIST(root="../Dataset/", download=True,
+dataset_fashionmnist = FashionMNIST(root="Dataset/", download=True,
                            transform=transforms.Compose([
                                transforms.Resize(28),
                                transforms.CenterCrop(28),
