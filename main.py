123f2f8b92ea6e419724fcf8ecdb3fc484647745
Karl-Liu-ch <liu_ht2010@qq.com>
2022-11-28 22:37:12 +0100

    add main.py

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ for i in range(1):
 for i in range(1):
     _WGAN = WGAN(ResNet=False, gradient_penalty=False, spectral_norm=True, train_set='CIFAR', iter=i)
     _WGAN.train(train_loader_cifar)
+# train DCGAN on cifar
+for i in range(1):
+    _DCGAN = DCGAN(ResNet=False, train_set='CIFAR', iter=i)
+    _DCGAN.train(train_loader_cifar)
 # train wgan on FashionMNIST
 for i in range(3):
     _WGAN = WGAN(ResNet=False, gradient_penalty=False, spectral_norm=False, train_set='FashionMNIST', iter=i)
