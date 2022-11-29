a31f6598e0143888714755b4625f05e5271bcee0
Karl-Liu-ch <liu_ht2010@qq.com>
2022-11-29 18:42:36 +0100

    change resnet

--- WGAN/ResNet.py
+++ WGAN/ResNet.py
@@ -11,7 +11,6 @@ class Res_Block(nn.Module):
             nn.ReLU(),
             nn.Conv2d(out_channel, out_channel, kernel_size=(3,3), stride=(1,1), padding=1),
             nn.BatchNorm2d(out_channel),
-            nn.ReLU()
         )
         self.extra = nn.Sequential()
         if in_channel != out_channel:
@@ -44,21 +43,13 @@ class ResNet(nn.Module):
         self.blk2 = Res_Block(out_channel, out_channel)
         self.blk3 = Res_Block(out_channel, out_channel)
         self.blk4 = Res_Block(out_channel, out_channel)
-        self.out = nn.Sequential(
-            nn.Conv2d(out_channel, out_channel, kernel_size=(3, 3), stride=(1, 1), padding=1),
-            nn.BatchNorm2d(out_channel),
-            nn.ReLU()
-        )
         self.Relu = nn.ReLU()
 
     def forward(self, x):
         out = self.Conv(x)
-        x = self.Conv_x(x)
+        # x = self.Conv_x(x)
         out = self.blk4(self.blk3(self.blk2(self.blk1(out))))
-        # out = self.blk3(self.blk2(self.blk1(out)))
-        # out = self.blk2(self.blk1(out))
-        out = self.Relu(x + out)
-        out = self.out(out)
+        # out = self.Relu(x + out)
         return out
 
 class Res_Block_D(nn.Module):
@@ -74,7 +65,6 @@ class Res_Block_D(nn.Module):
             nn.utils.parametrizations.spectral_norm(self.conv1),
             nn.ReLU(),
             nn.utils.parametrizations.spectral_norm(self.conv2),
-            nn.ReLU()
         )
         self.extra = nn.Sequential()
         if in_channel != out_channel:
@@ -110,18 +100,11 @@ class ResNet_D(nn.Module):
         self.blk2 = Res_Block_D(out_channel, out_channel)
         self.blk3 = Res_Block_D(out_channel, out_channel)
         self.blk4 = Res_Block_D(out_channel, out_channel)
-        self.out = nn.Sequential(
-            nn.utils.parametrizations.spectral_norm(self.conv3),
-            nn.ReLU()
-        )
         self.Relu = nn.ReLU()
 
     def forward(self, x):
         out = self.Conv(x)
-        x = self.Conv_x(x)
+        # x = self.Conv_x(x)
         out = self.blk4(self.blk3(self.blk2(self.blk1(out))))
-        # out = self.blk3(self.blk2(self.blk1(out)))
-        # out = self.blk2(self.blk1(out))
-        out = self.Relu(x + out)
-        out = self.out(out)
+        # out = self.Relu(x + out)
         return out
