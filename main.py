d4713c9d99c4ff41357a8900520c39f6acf4fc0c
Karl-Liu-ch <liu_ht2010@qq.com>
2022-11-29 22:12:59 +0100

    main

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from Data_loader import *
 from WGAN.WGAN_models import WGAN
 from DCGAN.DCGAN_models import DCGAN
 # train wgan on cifar
-for i in range(3):
-    _WGAN = WGAN(ResNet=False, gradient_penalty=False, spectral_norm=False, train_set='CIFAR', iter=i)
-    _WGAN.train(train_loader_cifar)
+# for i in range(3):
+#     _WGAN = WGAN(ResNet=False, gradient_penalty=False, spectral_norm=False, train_set='CIFAR', iter=i)
+#     _WGAN.train(train_loader_cifar)
 # train sn_wgan on cifar
 for i in range(3):
     _WGAN = WGAN(ResNet=False, gradient_penalty=False, spectral_norm=True, train_set='CIFAR', iter=i)
