b31d7443a6cf18c23e7f28e374d8c4cdc362cdcb
Karl-Liu-ch <liu_ht2010@qq.com>
2022-12-01 10:11:34 +0100

    resnet

--- WGAN/ResNet.py
+++ WGAN/ResNet.py
@@ -6,10 +6,12 @@ class Res_Block(nn.Module):
     def __init__(self, in_channel, out_channel):
         super(Res_Block, self).__init__()
         self.Conv = nn.Sequential(
-            nn.Conv2d(in_channel, out_channel, kernel_size=(3,3), stride=(1,1), padding=1),
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(in_channel, out_channel, kernel_size=(3,3), stride=(1,1), padding=0),
             nn.BatchNorm2d(out_channel),
             nn.ReLU(True),
-            nn.Conv2d(out_channel, out_channel, kernel_size=(3,3), stride=(1,1), padding=1),
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(out_channel, out_channel, kernel_size=(3,3), stride=(1,1), padding=0),
             nn.BatchNorm2d(out_channel),
         )
         self.extra = nn.Sequential()
@@ -28,12 +30,14 @@ class ResNet(nn.Module):
     def __init__(self, in_channel, out_channel):
         super(ResNet, self).__init__()
         self.Conv = nn.Sequential(
-            nn.Conv2d(in_channel, out_channel, kernel_size=(3, 3), stride=(1,1), padding=1),
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(in_channel, out_channel, kernel_size=(3, 3), stride=(1,1), padding=0),
             nn.BatchNorm2d(out_channel),
             nn.ReLU(True)
         )
         self.Conv_x = nn.Sequential(
-            nn.Conv2d(in_channel, out_channel, kernel_size=(3, 3), stride=(1, 1), padding=1),
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(in_channel, out_channel, kernel_size=(3, 3), stride=(1, 1), padding=0),
             nn.BatchNorm2d(out_channel),
             nn.ReLU(True)
         )
@@ -51,15 +55,17 @@ class ResNet(nn.Module):
 class Res_Block_D(nn.Module):
     def __init__(self, in_channel, out_channel):
         super(Res_Block_D, self).__init__()
-        self.conv1 = nn.Conv2d(in_channel, out_channel, kernel_size=(3,3), stride=(1,1), padding=1)
-        self.conv2 = nn.Conv2d(out_channel, out_channel, kernel_size=(3,3), stride=(1,1), padding=1)
+        self.conv1 = nn.Conv2d(in_channel, out_channel, kernel_size=(3,3), stride=(1,1), padding=0)
+        self.conv2 = nn.Conv2d(out_channel, out_channel, kernel_size=(3,3), stride=(1,1), padding=0)
         self.conv3 = nn.Conv2d(in_channel, out_channel, kernel_size=(1,1), stride=(1,1))
         nn.init.xavier_uniform_(self.conv1.weight.data, 1.)
         nn.init.xavier_uniform_(self.conv2.weight.data, 1.)
         nn.init.xavier_uniform_(self.conv3.weight.data, 1.)
         self.Conv = nn.Sequential(
+            nn.ReflectionPad2d(1),
             nn.utils.parametrizations.spectral_norm(self.conv1),
             nn.ReLU(True),
+            nn.ReflectionPad2d(1),
             nn.utils.parametrizations.spectral_norm(self.conv2),
         )
         self.extra = nn.Sequential()
@@ -76,17 +82,19 @@ class Res_Block_D(nn.Module):
 class ResNet_D(nn.Module):
     def __init__(self, in_channel, out_channel):
         super(ResNet_D, self).__init__()
-        self.conv1 = nn.Conv2d(in_channel, out_channel, kernel_size=(3, 3), stride=(1,1), padding=1)
-        self.conv2 = nn.Conv2d(in_channel, out_channel, kernel_size=(3, 3), stride=(1, 1), padding=1)
-        self.conv3 = nn.Conv2d(out_channel, out_channel, kernel_size=(3, 3), stride=(1, 1), padding=1)
+        self.conv1 = nn.Conv2d(in_channel, out_channel, kernel_size=(3, 3), stride=(1,1), padding=0)
+        self.conv2 = nn.Conv2d(in_channel, out_channel, kernel_size=(3, 3), stride=(1, 1), padding=0)
+        self.conv3 = nn.Conv2d(out_channel, out_channel, kernel_size=(3, 3), stride=(1, 1), padding=0)
         nn.init.xavier_uniform_(self.conv1.weight.data, 1.)
         nn.init.xavier_uniform_(self.conv2.weight.data, 1.)
         nn.init.xavier_uniform_(self.conv3.weight.data, 1.)
         self.Conv = nn.Sequential(
+            nn.ReflectionPad2d(1),
             nn.utils.parametrizations.spectral_norm(self.conv1),
             nn.ReLU(True)
         )
         self.Conv_x = nn.Sequential(
+            nn.ReflectionPad2d(1),
             nn.utils.parametrizations.spectral_norm(self.conv2),
             nn.ReLU(True)
         )
