1f6bf046aabc6c67417441486da29c2caf91879c
Karl-Liu-ch <liu_ht2010@qq.com>
2022-12-02 14:07:51 +0100

    train

--- WGAN_models.py
+++ WGAN_models.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('../')
+sys.path.append('./')
 import torch
 import os
 from torch.autograd import Variable
@@ -11,10 +11,11 @@ from WGAN.Discriminator import Discriminator_Res, Discriminator_wgan_28, Discrim
     Discriminator_SN_28, Discriminator_SN_32, Discriminator_SN_Res
 from WGAN.get_fid_score import get_fid
 import torchvision.transforms as transforms
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 class WGAN():
-    def __init__(self, ResNet = False, gradient_penalty = False, spectral_norm = False, train_set = 'MNIST', iter = 0):
+    def __init__(self, ResNet=False, gradient_penalty=False, spectral_norm=False, train_set='MNIST', iter=0, G_iter = int(1e5), D_iter = int(5)):
         self.ResNet = ResNet
         self.epoch = 0
         self.maxepochs = int(1e3)
@@ -22,7 +23,7 @@ class WGAN():
         self.Real_losses = []
         self.Fake_losses = []
         self.weight_cliping_limit = 0.01
-        self.D_iter = 2
+        self.D_iter = D_iter
         self.fid_score = []
         self.best_fid = 1e10
         self.lambda_term = 10
@@ -31,7 +32,7 @@ class WGAN():
         self.train_set = train_set
         self.iter = str(int(iter))
         self.path = 'WGAN'
-        self.generator_iters = int(1e5)
+        self.generator_iters = G_iter
         if train_set == 'CIFAR':
             self.invTrans = transforms.Compose([transforms.Normalize(mean=[0., 0., 0.],
                                                                      std=[1 / 0.247, 1 / 0.243, 1 / 0.261]),
@@ -91,7 +92,7 @@ class WGAN():
         print(self.G)
 
     def calculate_gradient_penalty(self, real_images, fake_images):
-        eta = torch.FloatTensor(real_images.size(0),1,1,1).uniform_(0,1).to(device)
+        eta = torch.FloatTensor(real_images.size(0), 1, 1, 1).uniform_(0, 1).to(device)
         eta = eta.expand(real_images.size(0), real_images.size(1), real_images.size(2), real_images.size(3))
 
         interpolated = eta * real_images + ((1 - eta) * fake_images)
@@ -104,9 +105,9 @@ class WGAN():
 
         # calculate gradients of probabilities with respect to examples
         gradients = autograd.grad(outputs=prob_interpolated, inputs=interpolated,
-                               grad_outputs=torch.ones(
-                                   prob_interpolated.size()).to(device),
-                               create_graph=True, retain_graph=True)[0]
+                                  grad_outputs=torch.ones(
+                                      prob_interpolated.size()).to(device),
+                                  create_graph=True, retain_graph=True)[0]
 
         grad_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean() * self.lambda_term
         return grad_penalty
@@ -124,54 +125,52 @@ class WGAN():
         self.G.train()
         self.D.train()
         self.data = self.get_infinite_batches(train_loader)
-        while self.epoch < self.maxepochs + 1:
-            for x, _ in train_loader:
-                images = self.data.__next__()
-                images = Variable(images).to(device)
-                x = Variable(x).to(device)
-                batch_size = x.size(0)
-                for p in self.D.parameters():
-                    p.requires_grad = True
-                for i in range(self.D_iter):
-                    # train the discreiminator
-                    self.D.zero_grad()
-                    if not self.spectral_norm:
-                        if not self.gradient_penalty:
-                            for p in self.D.parameters():
-                                p.data.clamp_(-self.weight_cliping_limit, self.weight_cliping_limit)
-                    D_real = self.D(x)
-                    loss_real = -D_real.mean(0).view(1)
-                    loss_real.backward()
-                    z = Variable(torch.randn((batch_size, 100, 1, 1))).to(device)
-                    x_fake = self.G(z).detach()
-                    loss_fake = self.D(x_fake)
-                    loss_fake = loss_fake.mean(0).view(1)
-                    loss_fake.backward()
-                    if not self.spectral_norm:
-                        if self.gradient_penalty:
-                            gp = self.calculate_gradient_penalty(x.data, x_fake.data)
-                            gp.backward()
-                    self.optim_D.step()
-                    loss_D = loss_fake + loss_real
-                    self.Real_losses.append(loss_real.item())
-                    self.Fake_losses.append(loss_fake.item())
-
+        while self.epoch < self.generator_iters+1:
+            images = self.data.__next__()
+            x = Variable(images).to(device)
+            batch_size = x.size(0)
+            for p in self.D.parameters():
+                p.requires_grad = True
+            for i in range(self.D_iter):
+                # train the discreiminator
+                self.D.zero_grad()
+                if not self.spectral_norm:
+                    if not self.gradient_penalty:
+                        for p in self.D.parameters():
+                            p.data.clamp_(-self.weight_cliping_limit, self.weight_cliping_limit)
+                D_real = self.D(x)
+                loss_real = -D_real.mean(0).view(1)
+                loss_real.backward()
                 z = Variable(torch.randn((batch_size, 100, 1, 1))).to(device)
-                self.G.zero_grad()
-                for p in self.D.parameters():
-                    p.requires_grad = False
-                x_fake = self.G(z)
-                loss_G = self.D(x_fake)
-                loss_G = -loss_G.mean(0).view(1)
-                # train the generator
-                loss_G.backward()
-                self.optim_G.step()
-                self.G_losses.append(loss_G.item())
-            print("epoch:{}, G_loss:{}".format(self.epoch, loss_G.cpu().detach().numpy()))
-            print("D_real_loss:{}, D_fake_loss:{}".format(loss_real.cpu().detach().numpy(),
-                                                                   loss_fake.cpu().detach().numpy()))
+                x_fake = self.G(z).detach()
+                loss_fake = self.D(x_fake)
+                loss_fake = loss_fake.mean(0).view(1)
+                loss_fake.backward()
+                if not self.spectral_norm:
+                    if self.gradient_penalty:
+                        gp = self.calculate_gradient_penalty(x.data, x_fake.data)
+                        gp.backward()
+                self.optim_D.step()
+                loss_D = loss_fake + loss_real
+                self.Real_losses.append(loss_real.item())
+                self.Fake_losses.append(loss_fake.item())
+                print("D_real_loss:{}, D_fake_loss:{}".format(loss_real.cpu().detach().numpy(),
+                                                          loss_fake.cpu().detach().numpy()))
 
-            if self.epoch % 20 == 0:
+            z = Variable(torch.randn((batch_size, 100, 1, 1))).to(device)
+            self.G.zero_grad()
+            for p in self.D.parameters():
+                p.requires_grad = False
+            x_fake = self.G(z)
+            loss_G = self.D(x_fake)
+            loss_G = -loss_G.mean(0).view(1)
+            # train the generator
+            loss_G.backward()
+            self.optim_G.step()
+            self.G_losses.append(loss_G.item())
+            print("epoch:{}/{}, G_loss:{}".format(self.epoch, self.generator_iters, loss_G.cpu().detach().numpy()))
+
+            if self.epoch % 1000 == 0:
                 self.save()
                 self.evaluate()
                 fid_score = get_fid(x, x_fake.detach())
@@ -194,11 +193,11 @@ class WGAN():
                     "optimizer_G": self.optim_G.state_dict(),
                     "losses_G": self.G_losses,
                     "FID scores": self.fid_score,
-                    "Best FID score": self.best_fid}, self.checkpoint+self.path+"G.pth")
+                    "Best FID score": self.best_fid}, self.checkpoint + self.path + "G.pth")
         torch.save({"D_state_dict": self.D.state_dict(),
                     "optimizer_D": self.optim_D.state_dict(),
                     "losses_fake": self.Fake_losses,
-                    "losses_real": self.Real_losses}, self.checkpoint+self.path+"D.pth")
+                    "losses_real": self.Real_losses}, self.checkpoint + self.path + "D.pth")
         if self.epoch == self.maxepochs:
             torch.save({"epoch": self.epoch,
                         "G_state_dict": self.G.state_dict(),
@@ -206,16 +205,16 @@ class WGAN():
                         "optimizer_G": self.optim_G.state_dict(),
                         "losses_G": self.G_losses,
                         "FID scores": self.fid_score,
-                        "Best FID score": self.best_fid}, self.checkpoint+self.path+"G_{}.pth".format(self.epoch))
+                        "Best FID score": self.best_fid}, self.checkpoint + self.path + "G_{}.pth".format(self.epoch))
             torch.save({"D_state_dict": self.D.state_dict(),
                         "optimizer_D": self.optim_D.state_dict(),
                         "losses_fake": self.Fake_losses,
-                        "losses_real": self.Real_losses}, self.checkpoint+self.path+"D_{}.pth".format(self.epoch))
-        print("model saved! path: "+self.path)
+                        "losses_real": self.Real_losses}, self.checkpoint + self.path + "D_{}.pth".format(self.epoch))
+        print("model saved! path: " + self.path)
 
     def load(self):
-        checkpoint_G = torch.load(self.checkpoint+self.path+"G.pth")
-        checkpoint_D = torch.load(self.checkpoint+self.path+"D.pth")
+        checkpoint_G = torch.load(self.checkpoint + self.path + "G.pth")
+        checkpoint_D = torch.load(self.checkpoint + self.path + "D.pth")
         self.epoch = checkpoint_G["epoch"]
         self.G.load_state_dict(checkpoint_G["G_state_dict"])
         self.G_best.load_state_dict(checkpoint_G["G_best_state_dict"])
@@ -227,7 +226,7 @@ class WGAN():
         self.optim_D.load_state_dict(checkpoint_D["optimizer_D"])
         self.Fake_losses = checkpoint_D["losses_fake"]
         self.Real_losses = checkpoint_D["losses_real"]
-        print("model loaded! path: "+self.path)
+        print("model loaded! path: " + self.path)
 
     def evaluate(self):
         self.load()
@@ -247,7 +246,7 @@ class WGAN():
         else:
             path = root + '_FashionMNIST/'
         try:
-            os.mkdir('Results/'+path)
+            os.mkdir('Results/' + path)
         except:
             pass
         with torch.no_grad():
@@ -258,45 +257,46 @@ class WGAN():
             else:
                 fake_img = fake_img.mul(0.5).add(0.5)
             grid = utils.make_grid(fake_img)
-            utils.save_image(grid, 'Results/'+path+'img_generatori_iter_{}.png'.format(self.epoch))
+            utils.save_image(grid, 'Results/' + path + 'img_generatori_iter_{}.png'.format(self.epoch))
+
 
 if __name__ == '__main__':
     # Train CIFAR on WGAN 3 times.
     for i in range(3):
-        _WGAN = WGAN(ResNet = False, gradient_penalty = False, spectral_norm = False, train_set = 'CIFAR', iter=i)
+        _WGAN = WGAN(ResNet=True, gradient_penalty=False, spectral_norm=True, train_set='CIFAR', iter=i)
         _WGAN.train(train_loader_cifar)
 
     # Train CIFAR on Spectral Normalization WGAN 3 times. Set spectral_norm to true
     for i in range(3):
-        _WGAN = WGAN(ResNet = False, gradient_penalty = False, spectral_norm = True, train_set = 'CIFAR', iter=i)
+        _WGAN = WGAN(ResNet=False, gradient_penalty=False, spectral_norm=True, train_set='CIFAR', iter=i)
         _WGAN.train(train_loader_cifar)
 
     # Train MNIST on WGAN 3 times.
     for i in range(3):
-        _WGAN = WGAN(ResNet = False, gradient_penalty = False, spectral_norm = False, train_set = 'MNIST', iter=i)
+        _WGAN = WGAN(ResNet=False, gradient_penalty=False, spectral_norm=False, train_set='MNIST', iter=i)
         _WGAN.train(train_loader_mnist)
 
     # Train MNIST on Spectral Normalization WGAN 3 times. Set spectral_norm to true
     for i in range(3):
-        _WGAN = WGAN(ResNet = False, gradient_penalty = False, spectral_norm = True, train_set = 'MNIST', iter=i)
+        _WGAN = WGAN(ResNet=False, gradient_penalty=False, spectral_norm=True, train_set='MNIST', iter=i)
         _WGAN.train(train_loader_mnist)
 
     # Train FashionMNIST on WGAN 3 times.
     for i in range(3):
-        _WGAN = WGAN(ResNet = False, gradient_penalty = False, spectral_norm = False, train_set = 'FashionMNIST', iter=i)
+        _WGAN = WGAN(ResNet=False, gradient_penalty=False, spectral_norm=False, train_set='FashionMNIST', iter=i)
         _WGAN.train(train_loader_fashionmnist)
 
     # Train FashionMNIST on Spectral Normalization WGAN 3 times. Set spectral_norm to true
     for i in range(3):
-        _WGAN = WGAN(ResNet = False, gradient_penalty = False, spectral_norm = True, train_set = 'FashionMNIST', iter=i)
+        _WGAN = WGAN(ResNet=False, gradient_penalty=False, spectral_norm=True, train_set='FashionMNIST', iter=i)
         _WGAN.train(train_loader_fashionmnist)
 
     # Train CIFAR on ResNet WGAN 3 times. Set ResNet to True
     for i in range(3):
-        _WGAN = WGAN(ResNet = True, gradient_penalty = False, spectral_norm = False, train_set = 'CIFAR', iter=i)
+        _WGAN = WGAN(ResNet=True, gradient_penalty=False, spectral_norm=False, train_set='CIFAR', iter=i)
         _WGAN.train(train_loader_cifar)
 
     # Train CIFAR on ResNet Spectral Normalization WGAN 3 times. Set ResNet to True. Set spectral_norm to true
     for i in range(3):
-        _WGAN = WGAN(ResNet = True, gradient_penalty = False, spectral_norm = True, train_set = 'CIFAR', iter=i)
+        _WGAN = WGAN(ResNet=True, gradient_penalty=False, spectral_norm=True, train_set='CIFAR', iter=i)
         _WGAN.train(train_loader_cifar)
