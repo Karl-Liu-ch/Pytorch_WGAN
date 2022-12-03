db163e2622dc396acea8af9d42329f3a01f98e59
Karl-Liu-ch <liu_ht2010@qq.com>
2022-12-03 10:45:52 +0100

    resnet

--- DCGAN/ResNet.py
+++ DCGAN/ResNet.py
@@ -102,12 +102,12 @@ class ResNet(nn.Module):
         self.Conv = nn.Sequential(
             nn.Conv2d(in_channel, out_channel, kernel_size=(3, 3), stride=(1,1), padding=(1,1)),
             nn.BatchNorm2d(out_channel),
-            # nn.ReLU(True)
+            nn.ReLU(True)
         )
         self.Conv_x = nn.Sequential(
             nn.Conv2d(in_channel, out_channel, kernel_size=(3, 3), stride=(1, 1), padding=(1,1)),
             nn.BatchNorm2d(out_channel),
-            # nn.ReLU(True)
+            nn.ReLU(True)
         )
         self.blk1 = Res_Block(out_channel, out_channel)
         self.blk2 = Res_Block(out_channel, out_channel)
@@ -117,9 +117,9 @@ class ResNet(nn.Module):
     def forward(self, x):
         out = self.Conv(x)
         x = self.Conv_x(x)
-        # out = self.blk4(self.blk3(self.blk2(self.blk1(out))))
+        out = self.blk4(self.blk3(self.blk2(self.blk1(out))))
         # out = self.blk3(self.blk2(self.blk1(out)))
-        out = self.blk2(self.blk1(out))
+        # out = self.blk2(self.blk1(out))
         return out + x
 
 class Res_Block_D(nn.Module):
@@ -158,11 +158,11 @@ class ResNet_D(nn.Module):
         nn.init.xavier_uniform_(self.conv3.weight.data, 1.)
         self.Conv = nn.Sequential(
             nn.utils.parametrizations.spectral_norm(self.conv1),
-            # nn.ReLU(True)
+            nn.ReLU(True)
         )
         self.Conv_x = nn.Sequential(
             nn.utils.parametrizations.spectral_norm(self.conv2),
-            # nn.ReLU(True)
+            nn.ReLU(True)
         )
         self.blk1 = Res_Block_D(out_channel, out_channel)
         self.blk2 = Res_Block_D(out_channel, out_channel)
@@ -172,7 +172,7 @@ class ResNet_D(nn.Module):
     def forward(self, x):
         out = self.Conv(x)
         x = self.Conv_x(x)
-        # out = self.blk4(self.blk3(self.blk2(self.blk1(out))))
+        out = self.blk4(self.blk3(self.blk2(self.blk1(out))))
         # out = self.blk3(self.blk2(self.blk1(out)))
-        out = self.blk2(self.blk1(out))
+        # out = self.blk2(self.blk1(out))
         return out + x
