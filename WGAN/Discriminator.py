bf2a12a8711df7ab4e2cf681db261633d95079f6
Karl-Liu-ch <liu_ht2010@qq.com>
2022-12-17 21:07:22 +0100

    discriminator

--- WGAN/Discriminator.py
+++ WGAN/Discriminator.py
@@ -100,23 +100,18 @@ class Discriminator_SN_28(nn.Module):
 class Discriminator_SN_32(nn.Module):
     def __init__(self, input_nums):
         super(Discriminator_SN_32, self).__init__()
-        self.conv1 = nn.Conv2d(input_nums, 256, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1))
-        self.conv2 = nn.Conv2d(256, 512, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1))
-        self.conv3 = nn.Conv2d(512, 1024, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1))
-        self.conv4 = nn.Conv2d(1024, 1, kernel_size=(4, 4), stride=(1,1), padding=0)
-        nn.init.xavier_uniform_(self.conv1.weight.data, 1.)
-        nn.init.xavier_uniform_(self.conv2.weight.data, 1.)
-        nn.init.xavier_uniform_(self.conv3.weight.data, 1.)
-        nn.init.xavier_uniform_(self.conv4.weight.data, 1.)
+        def Conv(input_nums, output_nums):
+            layer = []
+            layer.append(nn.utils.parametrizations.spectral_norm(
+                nn.Conv2d(input_nums, output_nums, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1))))
+            layer.append(nn.ReLU(True))
+            return layer
         self.Net = nn.Sequential(
-            nn.utils.parametrizations.spectral_norm(self.conv1),
-            nn.ReLU(True),
-            nn.utils.parametrizations.spectral_norm(self.conv2),
-            nn.ReLU(True),
-            nn.utils.parametrizations.spectral_norm(self.conv3),
-            nn.ReLU(True),
+            *Conv(input_nums, 256),
+            *Conv(256, 512),
+            *Conv(512, 1024),
         )
-        self.conv = nn.utils.spectral_norm(self.conv4)
+        self.conv = nn.utils.spectral_norm(nn.Conv2d(1024, 1, kernel_size=(4, 4), stride=(1, 1), padding=(0, 0)))
         # self.conv =  nn.Conv2d(1024, 1, kernel_size=(4, 4), stride=(1,1), padding=0)
         print("Discriminator_SN_32")
     def forward(self, input):
