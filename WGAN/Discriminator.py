6f408111d3b5737fc0f7a35f19fedaddab2f19cb
Karl-Liu-ch <liu_ht2010@qq.com>
2022-12-17 21:01:54 +0100

    discriminator

--- WGAN/Discriminator.py
+++ WGAN/Discriminator.py
@@ -88,17 +88,13 @@ class Discriminator_SN_28(nn.Module):
             *Conv(256, 512),
             nn.utils.parametrizations.spectral_norm(nn.Conv2d(512, 1024, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))),
             nn.ReLU(True),
-            # nn.utils.spectral_norm(self.conv),
+            nn.utils.spectral_norm(self.conv),
             # nn.Conv2d(1024, 1, kernel_size=(4, 4), stride=(1, 1), padding=(0, 0))
         )
-        self.flatten = nn.Flatten()
-        self.dense = nn.utils.spectral_norm(nn.Linear(1024 * 4 * 4, 1))
         print("Discriminator_SN_28")
 
     def forward(self, input):
         output = self.Net(input)
-        output = self.dense(self.flatten(output))
-        output = output.view(-1, 1, 1, 1)
         return output
 
 class Discriminator_SN_32(nn.Module):
@@ -108,12 +104,10 @@ class Discriminator_SN_32(nn.Module):
         self.conv2 = nn.Conv2d(256, 512, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1))
         self.conv3 = nn.Conv2d(512, 1024, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1))
         self.conv4 = nn.Conv2d(1024, 1, kernel_size=(4, 4), stride=(1,1), padding=0)
-        self.linear = nn.Linear(1024 * 4 * 4, 1)
         nn.init.xavier_uniform_(self.conv1.weight.data, 1.)
         nn.init.xavier_uniform_(self.conv2.weight.data, 1.)
         nn.init.xavier_uniform_(self.conv3.weight.data, 1.)
         nn.init.xavier_uniform_(self.conv4.weight.data, 1.)
-        nn.init.xavier_uniform_(self.linear.weight.data, 1.)
         self.Net = nn.Sequential(
             nn.utils.parametrizations.spectral_norm(self.conv1),
             nn.ReLU(True),
@@ -122,25 +116,19 @@ class Discriminator_SN_32(nn.Module):
             nn.utils.parametrizations.spectral_norm(self.conv3),
             nn.ReLU(True),
         )
-        # self.conv = nn.utils.spectral_norm(self.conv4)
+        self.conv = nn.utils.spectral_norm(self.conv4)
         # self.conv =  nn.Conv2d(1024, 1, kernel_size=(4, 4), stride=(1,1), padding=0)
-        self.flatten = nn.Flatten()
-        self.dense = nn.utils.spectral_norm(self.linear)
         print("Discriminator_SN_32")
     def forward(self, input):
         output = self.Net(input)
-        output = self.dense(self.flatten(output))
-        output = output.view(-1, 1, 1, 1)
-        # output = self.conv(output)
+        output = self.conv(output)
         return output
 
 class Discriminator_SN_Res(nn.Module):
     def __init__(self, input_nums):
         super(Discriminator_SN_Res, self).__init__()
         self.conv1 = nn.Conv2d(1024, 1, kernel_size=(4, 4), stride=(1,1), padding=0)
-        self.linear = nn.Linear(1024 * 4 * 4, 1)
         nn.init.xavier_uniform_(self.conv1.weight.data, 1.)
-        nn.init.xavier_uniform_(self.linear.weight.data, 1.)
         self.Net = nn.Sequential(
             SN_ResBlockDiscriminator(input_nums, 512, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1),
                                      activation=nn.ReLU(True)),
@@ -149,14 +137,10 @@ class Discriminator_SN_Res(nn.Module):
             SN_ResBlockDiscriminator(1024, 1024, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1),
                                      activation=nn.ReLU(True)),
         )
-        # self.conv = nn.utils.spectral_norm(self.conv1)
-        self.flatten = nn.Flatten()
-        self.dense = nn.utils.spectral_norm(self.linear)
+        self.conv = nn.utils.spectral_norm(self.conv1)
         print("Discriminator_SN_Res")
 
     def forward(self, input):
         output = self.Net(input)
-        output = self.dense(self.flatten(output))
-        output = output.view(-1, 1, 1, 1)
-        # output = self.conv(output)
+        output = self.conv(output)
         return output
