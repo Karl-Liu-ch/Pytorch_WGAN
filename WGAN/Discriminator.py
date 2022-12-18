a1e770183494a0a05ea995b747beff5a1833a691
Karl-Liu-ch <liu_ht2010@qq.com>
2022-12-18 10:15:16 +0100

    gan

--- WGAN/Discriminator.py
+++ WGAN/Discriminator.py
@@ -100,7 +100,7 @@ class Discriminator_SN_28(nn.Module):
     def forward(self, input):
         output = self.Net(input)
         return output
-
+'''
 class Discriminator_SN_32(nn.Module):
     def __init__(self, input_nums):
         super(Discriminator_SN_32, self).__init__()
@@ -123,7 +123,7 @@ class Discriminator_SN_32(nn.Module):
         output = self.Net(input)
         output = self.conv(output)
         return output
-
+'''
 class Discriminator_SN_Res(nn.Module):
     def __init__(self, input_nums):
         super(Discriminator_SN_Res, self).__init__()
@@ -144,3 +144,31 @@ class Discriminator_SN_Res(nn.Module):
         output = self.Net(input)
         output = self.conv(output)
         return output
+    
+leak = 0.1
+w_g = 4
+class Discriminator_SN_32(nn.Module):
+    def __init__(self, input_nums):
+        super(Discriminator_SN_32, self).__init__()
+
+        self.conv1 = nn.utils.spectral_norm(nn.Conv2d(input_nums, 64, 3, stride=1, padding=(1,1)))
+        self.conv2 = nn.utils.spectral_norm(nn.Conv2d(64, 64, 4, stride=2, padding=(1,1)))
+        self.conv3 = nn.utils.spectral_norm(nn.Conv2d(64, 128, 3, stride=1, padding=(1,1)))
+        self.conv4 = nn.utils.spectral_norm(nn.Conv2d(128, 128, 4, stride=2, padding=(1,1)))
+        self.conv5 = nn.utils.spectral_norm(nn.Conv2d(128, 256, 3, stride=1, padding=(1,1)))
+        self.conv6 = nn.utils.spectral_norm(nn.Conv2d(256, 256, 4, stride=2, padding=(1,1)))
+        self.conv7 = nn.utils.spectral_norm(nn.Conv2d(256, 512, 3, stride=1, padding=(1,1)))
+
+        self.fc = nn.utils.spectral_norm(nn.Linear(w_g * w_g * 512, 1))
+
+    def forward(self, x):
+        m = x
+        m = nn.LeakyReLU(leak)(self.conv1(m))
+        m = nn.LeakyReLU(leak)(self.conv2(m))
+        m = nn.LeakyReLU(leak)(self.conv3(m))
+        m = nn.LeakyReLU(leak)(self.conv4(m))
+        m = nn.LeakyReLU(leak)(self.conv5(m))
+        m = nn.LeakyReLU(leak)(self.conv6(m))
+        m = nn.LeakyReLU(leak)(self.conv7(m))
+
+        return self.fc(m.view(-1,w_g * w_g * 512))
--- WGAN/Generator.py
+++ WGAN/Generator.py
@@ -1,20 +1,23 @@
 import sys
+
 sys.path.append('../')
 import torch.nn as nn
 from WGAN.ResNet import *
 
+
 class Generator_28(nn.Module):
     def __init__(self, num_input, num_output):
         super(Generator_28, self).__init__()
+
         def Conv(input_nums, output_nums):
             layer = []
-            layer.append(nn.ConvTranspose2d(input_nums, output_nums, kernel_size=(4,4), stride=(2,2), padding=(1,1)))
+            layer.append(nn.ConvTranspose2d(input_nums, output_nums, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1)))
             layer.append(nn.BatchNorm2d(output_nums))
             layer.append(nn.ReLU(True))
             return layer
 
         self.Net = nn.Sequential(
-            nn.ConvTranspose2d(num_input, 1024, kernel_size=(4,4), stride=(1,1), padding=(0,0)),
+            nn.ConvTranspose2d(num_input, 1024, kernel_size=(4, 4), stride=(1, 1), padding=(0, 0)),
             nn.BatchNorm2d(1024),
             nn.ReLU(True),
             *Conv(1024, 512),
@@ -30,6 +33,8 @@ class Generator_28(nn.Module):
         output = self.Net(input)
         return output
 
+
+'''
 class Generator_32(nn.Module):
     def __init__(self, num_input, num_output):
         super(Generator_32, self).__init__()
@@ -55,20 +60,23 @@ class Generator_32(nn.Module):
     def forward(self, input):
         output = self.Net(input)
         return output
+'''
+
 
 class Generator_Res(nn.Module):
     def __init__(self, num_input, num_output):
         super(Generator_Res, self).__init__()
         self.Net = nn.Sequential(
-            ResBlockGenerator(num_input, 1024, kernel_size=(4,4), stride=(1,1), padding=(0,0), activation=nn.ReLU(True)),
+            ResBlockGenerator(num_input, 1024, kernel_size=(4, 4), stride=(1, 1), padding=(0, 0),
+                              activation=nn.ReLU(True)),
             # 1024 * 4 * 4
-            ResBlockGenerator(1024, 512, kernel_size=(4,4), stride=(2,2), padding=(1,1), activation=nn.ReLU(True)),
+            ResBlockGenerator(1024, 512, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), activation=nn.ReLU(True)),
             # 64 * 8 * 8
-            ResBlockGenerator(512, 256, kernel_size=(4,4), stride=(2,2), padding=(1,1), activation=nn.ReLU(True)),
+            ResBlockGenerator(512, 256, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), activation=nn.ReLU(True)),
             # 64 * 16 * 16
-            ResBlockGenerator(256, 64, kernel_size=(4,4), stride=(2,2), padding=(1,1), activation=nn.ReLU(True)),
+            ResBlockGenerator(256, 64, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), activation=nn.ReLU(True)),
             # 64 * 32 * 32
-            nn.ConvTranspose2d(64, num_output, kernel_size=(3,3),stride=(1,1),padding=(1,1)),
+            nn.ConvTranspose2d(64, num_output, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
             nn.Tanh()
         )
         print("Generator_Res")
@@ -76,3 +84,27 @@ class Generator_Res(nn.Module):
     def forward(self, input):
         output = self.Net(input)
         return output
+
+class Generator_32(nn.Module):
+    def __init__(self, z_dim, num_output):
+        super(Generator_32, self).__init__()
+        self.z_dim = z_dim
+
+        self.model = nn.Sequential(
+            nn.ConvTranspose2d(z_dim, 1024, 4, stride=1),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(),
+            nn.ConvTranspose2d(1024, 512, 4, stride=2, padding=(1, 1)),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.ConvTranspose2d(512, 256, 4, stride=2, padding=(1, 1)),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.ConvTranspose2d(256, 64, 4, stride=2, padding=(1, 1)),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, num_output, 3, stride=1, padding=(1, 1)),
+            nn.Tanh())
+
+    def forward(self, z):
+        return self.model(z)
